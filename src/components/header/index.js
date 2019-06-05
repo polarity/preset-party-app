d62f03a7033e3a4122cd83e524e7098a6ef2e088
Robert Agthe <robert@scriptshit.de>
2019-06-05 13:45:19 +0200

    better useful header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,16 @@
 import { h, render, Component } from 'preact'
 import style from './style'
+import {shell} from 'electron'
 
 export default class Header extends Component {
+  handleClick (link) {
+    shell.openExternal(link)
+  }
+
   render () {
     return (<div id='Header' class={style.header}>
-      <div class={style.inactive}>Login</div>
-      <div class={style.inactive}>Chat</div>
-      <div class={style.inactive}>Projects</div>
+      <div onClick={this.handleClick.bind(this, 'https://discord.gg/0g2ZPafIN3eWParf')} title='Bitwig Discord' class={style.inactive}>Discord</div>
+      <div onClick={this.handleClick.bind(this, 'https://sso.bitwig.com/login')} class={style.inactive}>Bitwig Account</div>
       <div class={style.active}>Presets</div>
     </div>
     )
